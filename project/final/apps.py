365d5f56001d26433984e55c6668033774c6e334
AliAgalarov <91954478+AliAgalarov@users.noreply.github.com>
2022-03-31 16:51:26 +0400

    Ali_D16
    Ali_D16

---
+++ project/final/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class FinalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'final'
+
+    def ready(self):
+        import final.signals
